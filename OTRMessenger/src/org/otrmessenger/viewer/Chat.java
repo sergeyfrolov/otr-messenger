bfbf198f06c59904d5bab21aec1f72432719773d
Shirly23 <Shirly.monteroquesada@colorado.edu>
2017-04-28 16:43:01 -0600

    New Chat Page

--- OTRMessenger/src/org/otrmessenger/viewer/Chat.java
+++ OTRMessenger/src/org/otrmessenger/viewer/Chat.java
@@ -3,26 +3,77 @@ import org.otrmessenger.viewer.Host;
 import org.otrmessenger.viewer.User;
 import org.otrmessenger.crypto.EncrypterAES;
 import org.otrmessenger.History;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 
 
 public class Chat  {
+	
 	private Host host;
 	private User other;
 	private History history;
 	private EncrypterAES AES;
+	private JFrame frame;
+	private JTextField messageField;
+	private static String othername="other"; //for testing
 	
-	private boolean draw(){
-		boolean confirm = false;
-		//TODO
-		return confirm;
+	
+	public static void main(String[] args) {
+		othername = "other";
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Chat window = new Chat(othername);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	public void clickSendButton(){
-		//TODO
+
+	/**
+	 * Create the application.
+	 */
+	public Chat(String name) {
+		initialize(name);
 	}
-	public void addMessage(String msgText){
-		//TODO
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize(String name) {
+		frame = new JFrame("Chatting with "+name);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextArea HistoryArea = new JTextArea();
+		HistoryArea.setLineWrap(true);
+		HistoryArea.setWrapStyleWord(true);
+		HistoryArea.setBounds(17, 17, 415, 169);
+		frame.getContentPane().add(HistoryArea);
+		
+		messageField = new JTextField();
+		messageField.setText("");
+		messageField.setBounds(17, 212, 323, 42);
+		frame.getContentPane().add(messageField);
+		messageField.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSend.setBounds(339, 217, 93, 35);
+		frame.getContentPane().add(btnSend);
 	}
-	public void setOther(User other){
-		//TODO
-	}
+
 }
