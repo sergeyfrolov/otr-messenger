b76faf418202087ef5a3f0e34aae3e7d59a28ff5
Shirly23 <Shirly.monteroquesada@colorado.edu>
2017-04-29 13:55:27 -0600

    AddFriend class corrected

--- OTRMessenger/src/org/otrmessenger/viewer/AddFriend.java
+++ OTRMessenger/src/org/otrmessenger/viewer/AddFriend.java
@@ -1,12 +1,19 @@
 package org.otrmessenger.viewer;
-
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AddFriend {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JButton btnAdd;
+	private JButton btnCancel;
 
 	/**
 	 * Launch the application.
@@ -35,9 +42,37 @@ public class AddFriend {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame = new JFrame("Add a Friend");
+		frame.setBounds(100, 100, 350, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(43, 62, 61, 16);
+		frame.getContentPane().add(lblName);
+		
+		textField = new JTextField();
+		textField.setBounds(119, 50, 194, 33);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//add a friend
+			}
+		});
+		btnAdd.setBounds(43, 117, 117, 29);
+		frame.getContentPane().add(btnAdd);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//cancel (close window)
+			}
+		});
+		btnCancel.setBounds(196, 117, 117, 29);
+		frame.getContentPane().add(btnCancel);
 	}
 
 }
